633ae5ed65e6dcc8e26e07c84f8838c0cd47d187
mditbbte <andrii.ds.kor@gmail.com>
2019-12-14 22:50:40 +0200

    Updates

---
+++ precache-manifest.ecd79c89bf444c509991e4ea12ab686e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb71ea488401ae83095733e480040b3",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "f3610a8826f9325fed66",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "03e789a64c39e7c24513",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.43141cab.chunk.css"
+  },
+  {
+    "revision": "f3610a8826f9325fed66",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.e6236598.chunk.js"
+  },
+  {
+    "revision": "03e789a64c39e7c24513",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.eb80991d.chunk.js"
+  },
+  {
+    "revision": "9e01eaad6504549adb24",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.7abd870e.js"
+  }
+]);
